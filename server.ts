900ad4abf2584b0d3d6d3bde905e3a8c7a1f5462
Lucas Deray <lucas.deray@greenscope.io>
2023-06-16 22:41:31 +0200

    add authentication routes and controller

--- server.ts
+++ server.ts
@@ -1,10 +1,16 @@
-import express from 'express'
+import { Request, Response, NextFunction } from 'express'
 import cors from 'cors'
+import express from 'express'
+import jwt from 'jsonwebtoken'
+
 import userRoutes from './routes/userRoutes'
 import chatroomRoutes from './routes/chatroomRoutes'
 import chatMessageRoutes from './routes/chatMessageRoutes'
 import scoreboardRoutes from './routes/scoreboardRoutes'
-import jwt from 'jsonwebtoken'
+
+interface AuthRequest extends Request {
+    userId?: string
+}
 
 const app = express()
 
@@ -12,17 +18,21 @@ app.use(cors())
 app.use(express.json())
 
 // Middleware for verifying JWT tokens
-app.use((req, res, next) => {
+app.use((req: AuthRequest, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split(' ')[1]
     if (token) {
-        jwt.verify(token, 'your-secret-key', (err: any, decoded: any) => {
-            if (err) {
-                return res.status(401).json({ error: 'Invalid token' })
-            } else {
-                req.userId = (decoded as any).userId
-                next()
+        jwt.verify(
+            token,
+            process.env.JWT_TOKEN as string,
+            (err: any, decoded: any) => {
+                if (err) {
+                    return res.status(401).json({ error: 'Invalid token' })
+                } else {
+                    req.userId = decoded.userId
+                    next()
+                }
             }
-        })
+        )
     } else {
         res.status(401).json({ error: 'Missing token' })
     }
@@ -33,12 +43,12 @@ app.use(chatMessageRoutes)
 app.use(userRoutes)
 app.use(scoreboardRoutes)
 
-app.use((err: any, req: any, res: any, next: any) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error(err.stack)
     res.status(500).send('Something broke!')
 })
 
-// for development only
+// For development only
 process.on('uncaughtException', (err) => {
     console.error('There was an uncaught error', err)
     process.exit(1)
