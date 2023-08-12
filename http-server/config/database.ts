6f4ad2b1d09075664ccc704d71237fae5d306b7a
Lucas Deray <lucas.deray@greenscope.io>
2023-08-12 22:17:12 +0200

    sequelize auth

--- http-server/config/database.ts
+++ http-server/config/database.ts
@@ -31,4 +31,15 @@ const sequelize = new Sequelize(
     }
 )
 
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log(
+            'Connection to the database has been established successfully.'
+        )
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err)
+    })
+
 export default sequelize
